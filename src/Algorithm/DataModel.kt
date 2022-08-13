4f0838b5568a8f30d26dc57d7faff5174a545bac
Mahmoudreza <m.mohammadian@gmail.com>
2022-08-13 15:30:00 +0430

    read file and parse in into ram

---
+++ src/Algorithm/DataModel.kt
@@ -0,0 +1,9 @@
+package Algorithm
+
+class DataModel{
+
+   var internalUtilityList: MutableList<Int> = mutableListOf()
+   var sumUtility: Int = -1
+   var crossUtilityList: MutableList<String> = mutableListOf()
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import Algorithm.Algorithm;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Initial");
+    public static void main(String[] args) throws IOException {
+        new Algorithm( fileToPath( "test_0.txt" ) );
     }
+
+    public static String fileToPath(String filename) throws UnsupportedEncodingException {
+        URL url = Main.class.getResource(filename);
+        return java.net.URLDecoder.decode(url.getPath(),"UTF-8");
+    }
+
 }
 
 
