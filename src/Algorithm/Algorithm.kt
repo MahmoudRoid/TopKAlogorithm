3c3d47d3dce69fa587d143ea5e862ccbc2227df0
Mahmoudreza <m.mohammadian@gmail.com>
2022-08-14 17:52:57 +0430

    calculate sum in last row of matrix

--- src/Algorithm/Algorithm.kt
+++ src/Algorithm/Algorithm.kt
@@ -1,20 +1,20 @@
 package Algorithm
 
-import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray
 import java.io.BufferedReader
 import java.io.FileReader
 import java.io.IOException
-import java.util.Arrays
 
 class Algorithm(private val filePath: String) {
 
     private val dataModelList = mutableListOf<DataModel>()
-    private lateinit var initialMatrix: Array<IntArray>
-    private var initialMatrixRowCount = 0
+    private lateinit var initialMatrix: Array<Array<Pair<InternalUtility, CrossUtility>>>
+    private var initialMatrixColumnCount = 0
 
     init {
         readFileUsingBufferedReader(filePath)
         makeInitialMatrix()
+        calculateEachMatrixColumnSum()
+        reorderInitialMatrix()
     }
 
     private fun readFileUsingBufferedReader(filePath: String) {
@@ -37,8 +37,8 @@ class Algorithm(private val filePath: String) {
                             // internal
                             for (i in internalItems.indices){
                                 utilityList.add(Pair(InternalUtility(internalItems[i].toInt()), CrossUtility(crossItems[i].toInt()) ))
-                                if (utilityList.size > initialMatrixRowCount)
-                                    initialMatrixRowCount = utilityList.size
+                                if (utilityList.size > initialMatrixColumnCount)
+                                    initialMatrixColumnCount = utilityList.size
                             }
                         }
                     )
@@ -50,13 +50,64 @@ class Algorithm(private val filePath: String) {
         }
     }
 
+
     private fun makeInitialMatrix() {
+
+        fun initialMatrixValue(i: Int) = Pair(InternalUtility(0),CrossUtility(0))
+
 
         if (dataModelList.isEmpty()){
             println("data model list is empty")
             return
         }
 
-        initialMatrix = Array(initialMatrixRowCount) { IntArray(initialMatrixRowCount) }
+        val firstRowValueList = dataModelList.flatMap { it.utilityList.map { it.first.value } }.distinct().sorted()
+        val initialMatrixFirstRowList = firstRowValueList.map { it-> Pair(InternalUtility(it), CrossUtility(0)) }
+        initialMatrixColumnCount = initialMatrixFirstRowList.size
+        initialMatrix = Array(dataModelList.size + 2) { Array(initialMatrixColumnCount, ::initialMatrixValue) }
+
+
+        // add first row into matrix
+        for (i in 0 until initialMatrixColumnCount)
+            initialMatrix[0][i] = initialMatrixFirstRowList[i]
+
+        // add other items based on first row
+        for (i in 1 .. dataModelList.size)
+            for (j in 0 until dataModelList[i-1].utilityList.size)
+                initialMatrix[i][ firstRowValueList.indexOf(dataModelList[i-1].utilityList[j].first.value) ] = dataModelList[i-1].utilityList[j]
+
     }
+
+    private fun  calculateEachMatrixColumnSum(){
+
+        var sum = 0
+        for (i in 0 until initialMatrixColumnCount){
+            for (j in 1 until initialMatrix.size){
+                sum += initialMatrix[j][i].second.value
+                initialMatrix[j][i] = Pair(InternalUtility(sum),CrossUtility(0))
+            }
+            sum = 0
+        }
+
+    }
+
+    private fun reorderInitialMatrix() {
+        val list = initialMatrix[dataModelList.size].map { it.first.value }.toList()
+        val orderedList = list.sorted()
+
+        for( i in list.indices){
+
+        }
+
+
+        println()
+    }
+
+
+
+/*    private fun swapMatrixColumn(ci: Int, cj: Int){
+        for (i in 0..initialMatrixColumnCount)
+            initialMatrix[i][ci] = initialMatrix[i][ci].first.value xor initialMatrix[i][cj].first.value xor initialMatrix[i][ci].first.value.also { initialMatrix[i][cj].first.value = it }
+    }*/
+
 }
